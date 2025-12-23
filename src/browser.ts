9c624d03b9e5ea6bd2226bcfc4aec9a0ffae29a9
The-Best-Codes <bestcodes.official@gmail.com>
2025-12-23 08:35:17 -0600

    updates

--- src/browser.ts
+++ src/browser.ts
@@ -1,4 +1,6 @@
 import * as BestBible from "./index";
 
-// Expose the functions to the global scope
-(window as any).BestBible = BestBible;
+// Expose the functions to the global scope (only if window is available)
+if (typeof window !== "undefined") {
+  (window as any).BestBible = BestBible;
+}
